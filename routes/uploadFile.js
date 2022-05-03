a5481aa86d3134b980acd9d7e109a82787dd3f3e
manjeete <manjeetkr2017@gmail.com>
2022-05-03 17:00:18 +0530

    multifile upload

--- routes/uploadFile.js
+++ routes/uploadFile.js
@@ -3,12 +3,31 @@ var router = express.Router();
 const {uploadBannerImage} = require("../utils/multer");
 
 
-
+// for single file upload
 router.post("/",[uploadBannerImage.single("file")],async(req,res) =>{
     try{
         res.status(200).json({
             status:true,
             image:`https://service-icon-admin.s3.ap-south-1.amazonaws.com/`+req.file.key
+        })
+
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({
+            status:false,
+            msg:err.message
+        })
+    }
+})
+
+
+router.post("/multi",[uploadBannerImage.array("file",2)],async(req,res) =>{
+    try{
+        res.status(200).json({
+            status:true,
+            file1:`https://service-icon-admin.s3.ap-south-1.amazonaws.com/`+req.files[0].key,
+            file2:`https://service-icon-admin.s3.ap-south-1.amazonaws.com/`+req.files[1].key,
+
         })
 
     }catch(err){
